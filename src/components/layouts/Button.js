338803df066f0a3d83fde5078f7d273f5484bbba
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-11-03 12:14:51 -0300

    01_Parametros do projeto
    Criado backend e projeto base

--- src/components/layouts/Button.js
+++
@@ -1,8 +0,0 @@
-import styles from './Button.module.css'
-
-export default function Button({text}) {
-    return(
-        <div className={styles.buttons}>
-            <button type="submit">{text}</button>
-        </div>)
-}
